908cc6947b13e9d87dea60c73fcb3f44a04a6ea8
shanjoel <shan.joeld@gmail.com>
2021-06-07 16:05:03 +0530

    intial commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Note() {
+  return (
+      <section className="container">
+      <div className="note">
+        <h1>JavaScript and React.js</h1>
+        <p>
+          This was an amazing bootcamp taken up by Shaurya Singha. We covered
+          everything from scratch including JavaScript and React.js.
+        </p>
+      </div>
+      </section>
+  );
+}
+
+export default Note;
